9b1589f549db112ec71a1ccfd88150b3317bb38d
Junaid7200 <junaidjaffery1@gmail.com>
2026-01-27 15:37:06 +0500

    Add BrandSlider component for brand showcase

---
+++ src/components/BrandSlider.tsx
@@ -0,0 +1,30 @@
+import BrandCard from './BrandCard'
+
+type Brand = {
+  id: string
+  name: string
+  category: string
+  description?: string
+}
+
+export default function BrandSlider({ brands }: { brands: Brand[] }) {
+  return (
+    <div className="overflow-x-auto pb-4">
+      <div className="flex gap-6 snap-x snap-mandatory">
+        {brands.map((brand) => (
+          <div
+            key={brand.id}
+            className="snap-start min-w-[80%] sm:min-w-[45%] lg:min-w-[30%]"
+          >
+            <BrandCard
+              id={brand.id}
+              name={brand.name}
+              category={brand.category}
+              description={brand.description}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
